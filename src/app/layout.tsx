02adf9fc9966421c3bfd8f96afd785b5ae5cfe0a
Irae Veras <irae.online@gmail.com>
2025-01-20 18:07:30 -0300

    feat: import da font inter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import '@fontsource/inter/400.css'; // Peso 400 (Normal)
+import '@fontsource/inter/500.css'; // Peso 500 (Médio)
+import '@fontsource/inter/700.css'; // Peso 700 (Negrito)
 import "./globals.css";
 
 const geistSans = Geist({
