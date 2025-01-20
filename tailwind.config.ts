68a2f2cdcdc69ae2214cd14e25cedfe5c790f719
Irae Veras <irae.online@gmail.com>
2025-01-20 18:06:35 -0300

    feat: configuração da tipografia

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,23 @@ export default {
         alert: '#F59E0B', // Laranja
         error: '#EF4444', // Vermelho
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Fonte principal
+        serif: ['Georgia', 'serif'], // Fonte alternativa
+        mono: ['Menlo', 'monospace'], // Fonte monoespaçada
+      },
+      fontSize: {
+        sm: ['14px', '20px'], // Pequena
+        base: ['16px', '24px'], // Base
+        lg: ['18px', '28px'], // Grande
+        xl: ['20px', '30px'], // Extra Grande
+        '2xl': ['24px', '36px'], // Extra Extra Grande
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+        bold: '700',
+      },
     },
   },
   plugins: [],
