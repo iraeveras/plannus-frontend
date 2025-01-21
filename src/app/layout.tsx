1afa824c95bb2af83159e17eedbfd01977166d32
Irae Veras <irae.online@gmail.com>
2025-01-21 19:29:24 -0300

    feat: alterações no componente sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,42 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+"use client"
+
+import { useState } from "react";
 import '@fontsource/inter/400.css'; // Peso 400 (Normal)
 import '@fontsource/inter/500.css'; // Peso 500 (Médio)
 import '@fontsource/inter/700.css'; // Peso 700 (Negrito)
-import "./globals.css";
 import Sidebar from "@/components/Sidebar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Plannus - Sistema de Orçamento",
-  description: "Sistema web projetado para gerenciar e controlar o orçamento empresarial anual.",
-};
+import { RiMenuUnfold3Line } from "react-icons/ri";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const [isMobileOpen, setIsMobileOpen] = useState(false);
+
   return (
     <html lang="en">
       <body className="flex h-screen bg-gray-100">
-        <Sidebar />        
-        <main className="flex-1 p-6 overflow-y-auto">{children}</main>
+        {/* Botão para abrir/fechar no mobile */}
+        <button
+          onClick={() => setIsMobileOpen(!isMobileOpen)}
+          className="fixed top-4 left-2 z-50 bg-primary text-white p-2 rounded md:hidden"
+        >
+          <RiMenuUnfold3Line />
+        </button>
+
+        {/* Sidebar */}
+        <div
+          className={`fixed md:static z-40 bg-gray-800 text-white transition-transform ${isMobileOpen ? "translate-x-0" : "-translate-x-full"
+            } md:translate-x-0`}
+        >
+          <Sidebar />
+        </div>
+
+        {/* Conteúdo Principal */}
+        <main className="flex-1 px-6 overflow-y-auto">{children}</main>
       </body>
     </html>
   );
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,23 +1,124 @@
+"use client"
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
+import { FaHome, FaBuilding, FaClipboardList } from "react-icons/fa";
+import { FaDownLeftAndUpRightToCenter } from "react-icons/fa6";
+import { FaUpRightAndDownLeftFromCenter } from "react-icons/fa6";
+import { FaChevronCircleRight } from "react-icons/fa";
 
 const Sidebar = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [activeMenu, setActiveMenu] = useState<string | null>(null);
+
+  // Persistencia de estado no LocalStorage
+  useEffect(() => {
+    const saveState = localStorage.getItem("sidebar-collapsed");
+    if (saveState) {
+      setIsCollapsed(JSON.parse(saveState));
+    }
+  }, []);
+
+  const toggleCollapse = () => {
+    setIsCollapsed(!isCollapsed);
+    localStorage.setItem("sidebar-collapsed", JSON.stringify(!isCollapsed));
+  };
+
+  const toggleMenu = (menu: string) => {
+    setActiveMenu((prev) => (prev === menu ? null : menu));
+  };
+
   const menuItems = [
-    { name: "Dashboard", path: "/dashboard" },
-    { name: "Premissas", path: "/premises" },
-    { name: "Empresas", path: "/companies" },
+    {
+      name: "Dashboard",
+      path: "/dashboard",
+      icon: <FaHome />,
+    },
+    {
+      name: "Cadastros",
+      icon: <FaClipboardList />,
+      submenus: [
+        { name: "Premissas", path: "/premises/new" },
+        { name: "Empresas", path: "/companies" },
+        { name: "Funcionários", path: "/employees" },
+      ],
+    },
+    {
+      name: "Relatórios",
+      icon: <FaBuilding />,
+      submenus: [
+        { name: "Premissas", path: "/premises" },
+        { name: "Realtório 2", path: "reports/2" },
+      ],
+    },
     // Adicione mais itens conforme necessário
   ];
 
   return (
-    <aside className="w-64 bg-gray-800 text-white h-screen p-4">
-      <h1 className="text-2xl font-bold mb-6">Plannus</h1>
-      <nav>
-        <ul className="space-y-4">
+    <aside className={`${isCollapsed ? "w-16" : "w-64"
+      } bg-gray-800 text-white h-screen transition-all duration-300 flex flex-col`}
+    >
+      {/* Logo e botão de colapso */}
+      <div className="relative flex items-center bg-gray-900 border-b border-gray-700 px-4 py-4">
+        {isCollapsed ? (
+          <img
+            src="/logo.png"
+            alt="Plannus Logo"
+            className="h-9 w-h-9 rounded-full"
+          />
+        ) : (
+          <div className="flex items-center space-x-2">
+            <div className="bg-white rounded-full p-1">
+              <img
+                src="/logo.png"
+                alt="Plannus Logo"
+                className="h-10 w-10 rounded-full"
+              />
+            </div>
+            <span className="text-xl font-bold text-white">Plannus</span>
+          </div>
+        )}
+
+        {/* Botão de colapso */}
+        <button
+          onClick={toggleCollapse}
+          className={`${isCollapsed
+            ? "absolute left-12 top-1/2 transform -translate-y-1/2 text-white p-2 bg-gray-900 rounded-full"
+            : "absolute right-2 top-1/2 transform -translate-y-1/2 text-white p-2 hover:bg-gray-700 rounded"}`}
+        >
+          {isCollapsed ? <FaChevronCircleRight /> : <FaDownLeftAndUpRightToCenter />}
+        </button>
+      </div>
+
+      {/* Menus */}
+      <nav className="flex-1">
+        <ul className="space-y-2">
           {menuItems.map((item) => (
-            <li key={item.name}>
-              <Link href={item.path} className="block px-4 py-2 rounded hover:bg-gray-700">
-                {item.name}
-              </Link>
+            <li key={item.name} className="group">
+              <div
+                onClick={() => toggleMenu(item.name)}
+                className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-700 rounded"
+              >
+                <span className="text-xl">{item.icon}</span>
+                {!isCollapsed && (
+                  <span className="ml-4 group-hover:underline">{item.name}</span>
+                )}
+              </div>
+              {/* Submenus */}
+              {item.submenus && activeMenu === item.name && (
+                <ul className={`${isCollapsed ? "hidden" : "pl-8"} space-y-2`}>
+                  {item.submenus.map((submenu) => (
+                    <li key={submenu.name}>
+                      <Link
+                        href={submenu.path}
+                        className="block px-4 py-2 hover:bg-gray-600 rounded"
+                      >
+                        {submenu.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
