03ee6c9d634681185b10c835f8e07819190f4643
Irae Veras <irae.online@gmail.com>
2025-01-21 19:28:23 -0300

    feat: criado metadata

---
+++ src/app/metadata.ts
@@ -0,0 +1,18 @@
+export const metadata = {
+    premises: {
+        title: "Cadastrar Premissa - Plannus",
+        description: "Página para cadastrar novas premissas no sistema Plannus.",
+    },
+    editPremise: {
+        title: "Editar Premissa - Plannus",
+        description: "Página para editar premissas existentes no sistema Plannus.",
+    },
+    dashboard: {
+        title: "Dashboard - Plannus",
+        description: "Visão geral do sistema Plannus.",
+    },
+    listPremises: {
+        title: "Lista de Premissa - Plannus",
+        description: "Lista de premissas cadastradas no Sistema Plannus.",
+    }
+};
