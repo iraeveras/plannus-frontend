bff63dfbf8c36271df8b8972f0ac4e377b45f80e
Irae Veras <irae.online@gmail.com>
2025-01-22 19:00:49 -0300

    feat: instalação do shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,42 +1,119 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	darkMode: ["class"],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#3B82F6', // Azul
-        secondary: '#10B981', // Verde
-        background: '#FFFFFF', // Branco
-        text: '#1F2937', // Cinza Escuro
-        alert: '#F59E0B', // Laranja
-        error: '#EF4444', // Vermelho
-      },
-      fontFamily: {
-        sans: ['Inter', 'sans-serif'], // Fonte principal
-        serif: ['Georgia', 'serif'], // Fonte alternativa
-        mono: ['Menlo', 'monospace'], // Fonte monoespaçada
-      },
-      fontSize: {
-        sm: ['14px', '20px'], // Pequena
-        base: ['16px', '24px'], // Base
-        lg: ['18px', '28px'], // Grande
-        xl: ['20px', '30px'], // Extra Grande
-        '2xl': ['24px', '36px'], // Extra Extra Grande
-      },
-      fontWeight: {
-        normal: '400',
-        medium: '500',
-        bold: '700',
-      },
+		// Or if using `src` directory:
+		"./src/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+    	extend: {
+    		colors: {
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			background: 'hsl(var(--background))',
+    			text: '#1F2937',
+    			alert: '#F59E0B',
+    			error: '#EF4444',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			},
+    			sidebar: {
+    				DEFAULT: 'hsl(var(--sidebar-background))',
+    				foreground: 'hsl(var(--sidebar-foreground))',
+    				primary: 'hsl(var(--sidebar-primary))',
+    				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+    				accent: 'hsl(var(--sidebar-accent))',
+    				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+    				border: 'hsl(var(--sidebar-border))',
+    				ring: 'hsl(var(--sidebar-ring))'
+    			}
+    		},
+    		fontFamily: {
+    			sans: [
+    				'Inter',
+    				'sans-serif'
+    			],
+    			serif: [
+    				'Georgia',
+    				'serif'
+    			],
+    			mono: [
+    				'Menlo',
+    				'monospace'
+    			]
+    		},
+    		fontSize: {
+    			sm: [
+    				'14px',
+    				'20px'
+    			],
+    			base: [
+    				'16px',
+    				'24px'
+    			],
+    			lg: [
+    				'18px',
+    				'28px'
+    			],
+    			xl: [
+    				'20px',
+    				'30px'
+    			],
+    			'2xl': [
+    				'24px',
+    				'36px'
+    			]
+    		},
+    		fontWeight: {
+    			normal: '400',
+    			medium: '500',
+    			bold: '700'
+    		},
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		}
+    	}
     },
-  },
-  plugins: [],
+	plugins: [require("tailwindcss-animate")],
 } satisfies Config;
