36a51fcd32ea61a3360f69d602b46665eee1c68c
Irae Veras <irae.online@gmail.com>
2025-01-22 19:02:54 -0300

    feat: criação e configuração do componente sidebar shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,29 @@
-"use client"
 
-import { useState } from "react";
-import '@fontsource/inter/400.css'; // Peso 400 (Normal)
-import '@fontsource/inter/500.css'; // Peso 500 (Médio)
-import '@fontsource/inter/700.css'; // Peso 700 (Negrito)
-import Sidebar from "@/components/Sidebar";
-import { RiMenuUnfold3Line } from "react-icons/ri";
+import { cookies } from "next/headers";
+
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 import "./globals.css";
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
 
-  const [isMobileOpen, setIsMobileOpen] = useState(false);
+  const cookieStore = await cookies()
+  const defaultOpen = cookieStore.get("sidebar:state")?.value === "true"
 
   return (
     <html lang="en">
-      <body className="flex h-screen bg-gray-100">
-        {/* Botão para abrir/fechar no mobile */}
-        <button
-          onClick={() => setIsMobileOpen(!isMobileOpen)}
-          className="fixed top-4 left-2 z-50 bg-primary text-white p-2 rounded md:hidden"
-        >
-          <RiMenuUnfold3Line />
-        </button>
-
-        {/* Sidebar */}
-        <div
-          className={`fixed md:static z-40 bg-gray-800 text-white transition-transform ${isMobileOpen ? "translate-x-0" : "-translate-x-full"
-            } md:translate-x-0`}
-        >
-          <Sidebar />
-        </div>
-
-        {/* Conteúdo Principal */}
-        <main className="flex-1 px-6 overflow-y-auto">{children}</main>
+      <body>
+        <SidebarProvider defaultOpen={defaultOpen}>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
