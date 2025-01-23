24259391373aee5620dc1e0ddf3726e3f3400735
Irae Veras <irae.online@gmail.com>
2025-01-23 20:37:04 -0300

    feat: alteração do lang

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default async function RootLayout({
   const defaultOpen = cookieStore.get("sidebar:state")?.value === "true"
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="pt-BR" suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
