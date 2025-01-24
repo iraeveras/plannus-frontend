0f095fcb1d8011c1c2b1ed77ef3186ad77d3c00e
Irae Veras <irae.online@gmail.com>
2025-01-24 18:43:28 -0300

    feat: criação do metadata especifico

---
+++ src/app/premises/new/layout.tsx
@@ -0,0 +1,10 @@
+// File: src/app/premises/new/layout.tsx
+import React from "react";
+export const metadata = {
+    title: "Cadastro de Premissas - Plannus",
+    description: "Adicione novas premissas ao sistema Plannus.",
+};
+
+export default function NewPremiseLayout({ children }: { children: React.ReactNode }) {
+    return <>{children}</>;
+}
