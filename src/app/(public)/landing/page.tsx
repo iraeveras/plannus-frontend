bc968648c9de2862953f2a6f63bbc2a43fc53b40
Irae Veras <irae.online@gmail.com>
2025-02-07 18:19:21 -0300

    feat: landing, avatar

---
+++ src/app/(public)/landing/page.tsx
@@ -0,0 +1,8 @@
+import { LoginForm } from "@/components/login-form";
+import { GalleryVerticalEnd } from "lucide-react";
+
+export default function Landing() {
+  return (
+    <div>Landing Page</div>
+  );
+}
