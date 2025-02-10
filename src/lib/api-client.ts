e01070b9423d68950649453dece0b2957cddd311
Irae Veras <irae.online@gmail.com>
2025-02-10 17:41:43 -0300

    feat: criado arquivo api-client separado

---
+++ src/lib/api-client.ts
@@ -0,0 +1,21 @@
+export async function signIn(username: string, password: string) {
+    try {
+        const res = await fetch("http://localhost:5000/users/login", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        })
+    
+        if (!res.ok) {
+            throw new Error("Usuário ou senha inválidos")
+        }
+
+        const data = await res.json()
+        localStorage.setItem("token", data.token) // Salva o token JWT
+        return { success: true, user: data.user }
+    } catch (error: any) {
+        return { success: false, message: error.message }
+    }
+}  
