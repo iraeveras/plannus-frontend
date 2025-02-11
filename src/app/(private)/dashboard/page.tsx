3813135cf2ebb074c2141e7eb6b2ce6e5b64ca61
Irae Veras <irae.online@gmail.com>
2025-02-11 12:47:19 -0300

    feat: import do component Header

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -2,6 +2,7 @@
 // File: src/app/dashboard/page.tsx
 
 import { metadata } from "@/app/metadata";
+import HeaderPage from "@/components/private/header-page";
 import { useEffect } from "react";
 
 export default function Dashboard() {
@@ -14,22 +15,28 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <main className="p-6 bg-gray-100 min-h-screen">
-      <h1 className="text-3xl font-bold text-primary">Dashboard</h1>
-      <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="p-4 bg-white rounded shadow">
-          <h2 className="text-lg font-bold text-secondary">Resumo Financeiro</h2>
-          <p className="mt-2 text-gray-700">Total de Orçamento: R$ 1.000.000,00</p>
-        </div>
-        <div className="p-4 bg-white rounded shadow">
-          <h2 className="text-lg font-bold text-secondary">Funcionários</h2>
-          <p className="mt-2 text-gray-700">Total: 120</p>
-        </div>
-        <div className="p-4 bg-white rounded shadow">
-          <h2 className="text-lg font-bold text-secondary">Empresas</h2>
-          <p className="mt-2 text-gray-700">Total: 10</p>
+    <>
+      <HeaderPage
+        breadcrumbItems={[{ label: "Dashboard", href: "/" }]}
+      //currentPage="Home"
+      />
+      <div className="p-6">
+        {/* <h1 className="text-3xl font-bold text-primary">Dashboard</h1> */}
+        <div className="mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="p-4 bg-white rounded shadow">
+            <h2 className="text-lg font-bold text-secondary">Resumo Financeiro</h2>
+            <p className="mt-2 text-gray-700">Total de Orçamento: R$ 1.000.000,00</p>
+          </div>
+          <div className="p-4 bg-white rounded shadow">
+            <h2 className="text-lg font-bold text-secondary">Funcionários</h2>
+            <p className="mt-2 text-gray-700">Total: 120</p>
+          </div>
+          <div className="p-4 bg-white rounded shadow">
+            <h2 className="text-lg font-bold text-secondary">Empresas</h2>
+            <p className="mt-2 text-gray-700">Total: 10</p>
+          </div>
         </div>
       </div>
-    </main>
+    </>
   );
 }  
