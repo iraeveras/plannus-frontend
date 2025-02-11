cf976409c61240d2a7a551f9eabbac6e08e5468b
Irae Veras <irae.online@gmail.com>
2025-02-11 17:27:28 -0300

    feat: constant array slide

---
+++ src/constants/slides.ts
@@ -0,0 +1,28 @@
+const slides = [
+    {
+        id: 1,
+        title: 'Planejamento Financeiro',
+        image: 'https://images.unsplash.com/photo-1556740749-887f6717d7e4?auto=format&fit=crop&w=1950&q=80', // Exemplo de imagem de escritório
+        subtitle: 'Estratégia e análise para o sucesso',
+    },
+    {
+        id: 2,
+        title: 'Reunião de Equipe',
+        image: 'https://images.unsplash.com/photo-1551836022-d5d88e9218df?auto=format&fit=crop&w=1950&q=80', // Exemplo de reunião
+        subtitle: 'Colaboração para decisões acertadas',
+    },
+    {
+        id: 3,
+        title: 'Análise de Dados',
+        image: 'https://images.unsplash.com/photo-1519389950473-47ba0277781c?auto=format&fit=crop&w=1950&q=80', // Exemplo de análise de dados
+        subtitle: 'Informações precisas para o controle orçamentário',
+    },
+    {
+        id: 4,
+        title: 'Revisão Financeira',
+        image: 'https://images.pexels.com/photos/3183153/pexels-photo-3183153.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', // Exemplo de análise de dados
+        subtitle: 'Informações precisas para o controle orçamentário',
+    },
+];
+
+export default slides;
