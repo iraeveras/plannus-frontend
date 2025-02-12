b9256c8f97efc9743c7f48fa963f3cfe4cd27d27
Irae Veras <irae.online@gmail.com>
2025-02-12 00:04:36 -0300

    feat: adicionado interceptador que adiciona o token para cada requisição

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,4 +5,15 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+// Interceptador que adiciona o token a cada requisição
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default api;
