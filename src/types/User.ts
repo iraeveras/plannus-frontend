7f410d6900bf1f26cf2b6ec2cacb05645a597336
Irae Veras <irae.online@gmail.com>
2025-02-12 18:22:12 -0300

    feat: adicionado tipágem

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,9 +1,11 @@
 // src/types/User.ts
 
+export type UserRole = "admin" | "managerGeafi" | "managerGerop" | "managerGemkt" | "supervisor" | "user";
+
 export interface User {
     id: string;
     name: string;
     email: string;
-    role: string;
+    role: UserRole;
     avatar?: string;
 }
