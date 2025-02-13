482447461751686770bf662029c9762fb3c2d95e
Irae Veras <irae.online@gmail.com>
2025-02-13 00:39:58 -0300

    feat: inclusão do metadata para as pages users

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -7,20 +7,32 @@ export const metadata = {
         title: "Login - Sistema Plannus",
         description: "Página de login - Sistema Plannus"
     },
+    user: {
+        title: "Cadastrar Usuário - Sistema Plannus",
+        description: "Página de login - Sistema Plannus"
+    },
+    EditUser: {
+        title: "Editar Usuário - Sistema Plannus",
+        description: "Página de login - Sistema Plannus"
+    },
+    listUsers: {
+        title: "Usuário - Sistema Plannus",
+        description: "Página de login - Sistema Plannus"
+    },
     premises: {
-        title: "Cadastrar Premissa - Plannus",
+        title: "Cadastrar Premissa - Sistema Plannus",
         description: "Página para cadastrar novas premissas no sistema Plannus.",
     },
     editPremise: {
-        title: "Editar Premissa - Plannus",
+        title: "Editar Premissa - Sistema Plannus",
         description: "Página para editar premissas existentes no sistema Plannus.",
     },
     dashboard: {
-        title: "Dashboard - Plannus",
+        title: "Dashboard - Sistema Plannus",
         description: "Visão geral do sistema Plannus.",
     },
     listPremises: {
-        title: "Lista de Premissa - Plannus",
+        title: "Lista de Premissas - Sistema Plannus",
         description: "Lista de premissas cadastradas no Sistema Plannus.",
     }
 };
