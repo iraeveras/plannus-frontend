26222df0ee88365aebd3c7710ebac3607ff9bf77
Irae Veras <irae.online@gmail.com>
2025-02-17 19:04:51 -0300

    feat: import do component page PermissionTypes

--- src/app/(private)/(admin)/settings/page.tsx
+++ src/app/(private)/(admin)/settings/page.tsx
@@ -6,6 +6,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { TabItem } from "@/components/forms/tabs/tab-item";
 import HeaderPage from "@/components/private/header-page";
 import UserConfigurationsTabs from "./user/user-configurations-tabs";
+import PermissionTypes from "./permissions/permission-types";
 
 export default function SettingsPage() {
     return (
@@ -18,16 +19,16 @@ export default function SettingsPage() {
                 <h1 className="text-xl font-medium text-zinc-900 dark:text-zinc-100 mb-4">⚙️ Configurações</h1>
                 <Tabs defaultValue="users">
                     <TabsList className="flex text-neutral-400 scroll-smooth">
+                        <TabItem label="Tipos de permissões" value="permissionType" />
                         <TabItem label="Usuários" value="users" />
-                        <TabItem label="Conta" value="account" />
                         <TabItem label="Notificações" value="notifications" />
                         <TabItem label="Outras" value="others" />
                     </TabsList>
+                    <TabsContent value="permissionType" className="p-3 transition-opacity duration-300 ease-in-out">
+                        <PermissionTypes />
+                    </TabsContent>
                     <TabsContent value="users" className="py-3 transition-opacity duration-300 ease-in-out">
                         <UserConfigurationsTabs />
-                    </TabsContent>
-                    <TabsContent value="account" className="p-3 transition-opacity duration-300 ease-in-out">
-                        <p>Configurações da conta...</p>
                     </TabsContent>
                     <TabsContent value="notifications" className="p-3 transition-opacity duration-300 ease-in-out">
                         <p>Configurações de notificações...</p>
@@ -38,6 +39,6 @@ export default function SettingsPage() {
                 </Tabs>
             </div>
         </>
-        
+
     );
 }
