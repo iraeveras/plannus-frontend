eb4c365e61cc1ceca69d5bb49a66376f45008109
Irae Veras <irae.online@gmail.com>
2025-02-17 19:02:24 -0300

    feat: import dos components UserConfigContent e AssignPermissionsPage

--- src/app/(private)/(admin)/settings/user/user-configurations-tabs.tsx
+++ src/app/(private)/(admin)/settings/user/user-configurations-tabs.tsx
@@ -6,6 +6,7 @@ import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 import { TabSubItem } from "@/components/forms/tabs/tab-subitem";
 import UserConfigContent from "./user-config-content";
 import { Dot } from "lucide-react"
+import AssignPermissionsPage from "./user-config-permissions";
 
 export default function UserConfigurationsTabs() {
     return (
@@ -17,11 +18,11 @@ export default function UserConfigurationsTabs() {
             </TabsList>
             <TabsContent value="list" className="p-3 transition-opacity duration-300 ease-in-out">
                 {/* Aqui você renderiza o componente ou conteúdo de listagem de usuários */}
-                <UserConfigContent/>
+                <UserConfigContent />
             </TabsContent>
             <TabsContent value="permissions" className="p-3 transition-opacity duration-300 ease-in-out">
                 {/* Aqui você renderiza o componente ou formulário para atribuição de permissões */}
-                <p>Aqui vai a configuração de permissões para usuários.</p>
+                <AssignPermissionsPage />
             </TabsContent>
             <TabsContent value="reset" className="p-3 transition-opacity duration-300 ease-in-out">
                 {/* Aqui você renderiza o formulário de reset de senha */}
