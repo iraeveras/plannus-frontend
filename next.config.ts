1122d98d96d4e539b3999d6497aefe36c7c52c4c
Irae Veras <irae.online@gmail.com>
2025-02-27 14:36:33 -0300

    feat: configurações para permissões da URL da imagem

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['images.unsplash.com', 'images.pexels.com', 'localhost'],
+    domains: ['images.unsplash.com', 'images.pexels.com', 'localhost', "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+    ],
   },
 };
 
