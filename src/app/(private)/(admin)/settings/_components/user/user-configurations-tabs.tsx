74b8ae61c1bcd50bedb4e1b697ce0a463ef0848c
Irae Veras <irae.online@gmail.com>
2025-03-06 01:08:40 -0300

    feat: ajuste de posicinamento de Tabs

--- src/app/(private)/(admin)/settings/_components/user/user-configurations-tabs.tsx
+++ src/app/(private)/(admin)/settings/_components/user/user-configurations-tabs.tsx
@@ -13,20 +13,20 @@ export default function UserConfigurationsTabs() {
         <Tabs defaultValue="list" className="px-0 mx-0">
             <TabsList className="flex scroll-smooth rounded-none tracking-wide px-1 text-neutral-400 dark:text-neutral-600 bg-transparent dark:bg-transparent">
                 <TabSubItem label="• Listar Usuários" value="list" />
-                <TabSubItem label="• Atribuir Permissões" value="permissions" />
                 <TabSubItem label="• Reset de Senha" value="reset" />
+                <TabSubItem label="• Atribuir Permissões" value="permissions" />
             </TabsList>
             <TabsContent value="list" className="py-1 transition-opacity duration-300 ease-in-out">
                 {/* Aqui você renderiza o componente ou conteúdo de listagem de usuários */}
                 <UserConfigContent />
             </TabsContent>
-            <TabsContent value="permissions" className="py-1 transition-opacity duration-300 ease-in-out">
-                {/* Aqui você renderiza o componente ou formulário para atribuição de permissões */}
-                <AssignPermissionsPage />
-            </TabsContent>
             <TabsContent value="reset" className="py-1 transition-opacity duration-300 ease-in-out">
                 {/* Aqui você renderiza o formulário de reset de senha */}
                 <p>Aqui vai o formulário de reset de senha.</p>
+            </TabsContent>
+            <TabsContent value="permissions" className="py-1 transition-opacity duration-300 ease-in-out">
+                {/* Aqui você renderiza o componente ou formulário para atribuição de permissões */}
+                <AssignPermissionsPage />
             </TabsContent>
         </Tabs>
     );
